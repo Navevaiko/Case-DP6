281bb127c39b4c93a9a07f449ce361592d091b67
Gabriel Navevaiko <gabriel_navevaiko@hotmail.com>
2022-01-17 22:06:47 -0300

    feat: Adicionando eventos do Google Analytics

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -1,6 +1,50 @@
-// Preencha este arquivo com qualquer código que você necessite para realizar a
-// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
-// Manager. No último caso, não é necessário implementar a tag <noscript>.
-// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
-// para fazer a sua coleta.
-// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-12345-6', 'auto');
+
+// Adicionando evento de visualização da página ao inicia-lá
+ga('send', 'pageview', location.pathname);
+
+function sendEvent(eventCategory, eventAction, eventLabel) {
+  ga('send', {
+    hitType: 'event',
+    eventCategory: eventCategory,
+    eventAction: eventAction,
+    eventLabel: eventLabel
+  });
+}
+
+// Enviando evento ao clicar no menu de contato
+jQuery('.menu-lista-contato').click(() => {
+  sendEvent(eventCategory='menu', eventAction='entre_em_contato', eventLabel='link_externo');
+});
+
+// Enviando evento ao clicar no menu de download
+jQuery('.menu-lista-download').click(() => {
+  sendEvent(eventCategory='menu', eventAction='download_pdf', eventLabel='download_pdf');
+});
+
+// Enviando evento quando o usuário clica nos cards da página analise
+jQuery('.card').click((event) => {
+  var name = jQuery(event.currentTarget).data('name');
+
+  sendEvent(eventCategory='analise', eventAction='ver_mais', eventLabel=name);
+});
+
+// Enviando evento quando o usuário preenche um campo ou envia o formulário
+jQuery('.contato :input').each((_, element) => {
+  if(element.type !== 'submit') {
+    jQuery(element).blur(() => {
+      if(element.value !== '') {
+        sendEvent(eventCategory='contato', eventAction=element.id, eventLabel='preencheu');
+      }
+    });
+  } else {
+    jQuery(element).click(() => {
+      sendEvent(eventCategory='contato', eventAction='enviado', eventLabel='enviado');
+    });
+  }
+})
